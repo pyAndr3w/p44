a07ef4963d79649c32607c10fd88731405dd76d1
Andrew <gutarev01@gmail.com>
2023-05-21 01:08:07 +0600

    fix paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const isProduction = process.argv.includes('production')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProduction ? '/p44/' : '/',
   build: {
     outDir: "./dist"
   },
